ba47f4f1dbf8631f01d720ba7a00e1d6b641bd1a
DevJohanR <desarrollador7777@gmail.com>
2024-05-30 21:37:19 -0500

    se agrega cors restringida para dominio especifico

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,22 @@
-// src/index.js
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const projectsRouter = require('./routes/projects');
 require('dotenv').config();
+
+const app = express();
+
+const allowedOrigins = ['https://johanriascos.rf.gd'];
+app.use(cors({
+  origin: function (origin, callback) {
+    // Permitir solicitudes sin origen (como las herramientas Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(express.json());
 app.use('/projects', projectsRouter);
