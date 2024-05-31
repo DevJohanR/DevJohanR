2f21c47e467618f329f60065c414d0d8d41745a4
DevJohanR <desarrollador7777@gmail.com>
2024-05-30 23:13:28 -0500

    Se agrega boton de WhatsApp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
 import Header from './components/Header/Header';
 import Home from './components/Home/Home'; // Asegúrate de tener estos componentes
 import About from './components/About/About';
 import Services from './components/Services/Services';
 import Contact from './components/Contact/Contact';
 import Alert from './components/Alert';
+import foto from './assets/img/johan.jpg';
 
 
 const App = () => {
@@ -21,6 +23,15 @@ const App = () => {
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
+      <FloatingWhatsApp
+      phoneNumber='+573027777134'
+      accountName='Johan Riascos'
+      avatar={foto}
+      statusMessage='Responde inmediatamente'
+      chatMessage='Hola! Me gustaria ser parte de tu equipo! ¿Hablamos?'
+      notification={true}
+    />
     </div>
   );
 };
